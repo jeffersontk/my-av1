9ea42e4673ee6a52ce1a29bf2234cc3d07153dea
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-04-03 15:36:25 -0300

    AV1 de dev web
    Desenvolva um portal com HTML, CSS e Javascript que calcule o consumo de um veículo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tank, setTank] = useState("");
+  const [km, setKm] = useState("");
+  const [result, setResult] = useState("");
+
+  function consumed(e) {
+    e.preventDefault();
+
+    let contador = 0;
+    const calc = km / tank;
+
+    const countdown = setInterval(function () {
+      setResult(contador);
+
+      if (contador >= 100) {
+        clearInterval(countdown);
+        return setResult(calc.toFixed(1));
+      }
+      contador++;
+    }, 20);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>Medidor de consumo de combustivel por km rodado</h1>
+        <p>AV1 Desenvolvimento Web por Jefferson brito</p>
+        <div className="display">
+          <div className="result">{result}</div>
+          <div className="pointer"></div>
+        </div>
+        <form>
+          <input
+            placeholder="Combustivel no tanque que irá ser usada na viagem"
+            required
+            type="number"
+            value={tank}
+            onChange={(e) => setTank(e.target.value)}
+          ></input>
+          <input
+            placeholder="KM percorrido na viagem"
+            required
+            type="number"
+            value={km}
+            onChange={(e) => setKm(e.target.value)}
+          ></input>
+
+          <button type="submit" onClick={consumed}>
+            verificar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
